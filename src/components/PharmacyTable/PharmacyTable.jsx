de3b4f5d962646867248b42f2ff4ad28c1b56dac
atilagulers <atilagulerofficial@gmail.com>
2023-10-19 00:40:33 +0300

    .

--- src/components/PharmacyTable/PharmacyTable.jsx
+++ src/components/PharmacyTable/PharmacyTable.jsx
@@ -124,7 +124,7 @@ function PharmacyTable({pharmacies}) {
                 ></script>
                 <ins
                   class="adsbygoogle"
-                  style="display:block"
+                  style={{display: 'block'}}
                   data-ad-format="fluid"
                   data-ad-layout-key="-6t+ed+2i-1n-4w"
                   data-ad-client="ca-pub-3435885379042213"
